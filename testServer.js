5ba9cc3e0cb91f6501f9b416a5d32abc9daa5754
Allan Rodríguez <allanrod95@yahoo.com>
2017-10-24 19:49:35 -0700

    Added shadow on the header. Fixed horizontal bar below not showing up. Added mobile format support. Added MaxLite and Jelly info on résumé page. Update résumé PDF.

--- testServer.js
+++ testServer.js
@@ -11,12 +11,24 @@ http.createServer(function (req, res) {
 		});
 	} else {
 		fs.readFile('./' + req.url, function(err, data) {
-			if (req.url.search(".css") < 0) {
-				res.writeHead(200, {'Content-Type': 'text/html'});
+			if (err == null) {
+				if (req.url.search("\\.css") == req.url.length - 4) {
+					console.log("css");
+					res.writeHead(200, {'Content-Type': 'text/css'});
+				} else if (req.url.search("\\.pdf") == req.url.length - 4) {
+					console.log("pdf");
+					res.writeHead(200, {'Content-Type': 'application/pdf'});
+				} else if (req.url.search("\\.js") == req.url.length - 3) {
+					console.log("js");
+					res.writeHead(200, {'Content-Type': 'application/javascript'});
+				} else {
+					console.log("html");
+					res.writeHead(200, {'Content-Type': 'text/html'});
+				}
+				res.write(data);
 			} else {
-				res.writeHead(200, {'Content-Type': 'text/css'});
+				console.log(req.url + " not found!");
 			}
-			res.write(data);
 			res.end();
 		});
 	}
