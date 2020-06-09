815d90dc58cbd432dba0d9470e12fefc96c9ee41
Allan Rodríguez <allanr1@uci.edu>
2020-06-08 22:14:03 -0700

    Added Bootstrap and Normalize.

---
+++ src/components/header/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from "./header.module.css"
+
+function Header() {
+  return (
+    <div className={styles.titleContainer}>
+      <div className={styles.title}>Allan Rodríguez</div>
+    </div>
+  )
+}
+
+export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,32 @@
 import React from "react"
+import Header from "../components/header"
+import "normalize.css"
+import "bootstrap/dist/css/bootstrap-grid.css"
+import "./global.css"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+      <div className="container-md">
+        <div
+          style={{
+            fontSize: "5rem",
+            fontWeight: "900",
+            textAlign: "center"
+          }}
+        >
+          Allan Rodríguez
+        </div>
+        <h2>Hello world!</h2>
+        My name is Allan Rodríguez, and I'm 23 years old as of late 2018. I
+        graduated with a Bachelor's Degree in Computer Engineering from UC
+        Irvine in June of 2017. Ever since, I've been using my skills for work
+        as well as on small personal projects, and to learn more outside of what
+        school has taught me.
+      </div>
+    </>
+  )
 }
