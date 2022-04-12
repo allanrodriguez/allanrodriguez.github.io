dc5da3636738ca6d301203dba454be20764b0ad2
Allan Rodríguez <allanr1@uci.edu>
2022-04-12 01:17:57 -0700

    Updated footer copyright year.

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <div className="py-3 text-center text-muted">
-      <small>&copy; 2021 Allan Rodriguez</small>
+      <small>&copy; 2022 Allan Rodriguez</small>
     </div>
   );
 };
